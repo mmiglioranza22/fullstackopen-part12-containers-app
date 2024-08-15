a891759efaa53918c16d448f7e362f9ee224a806
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-08-15 12:01:19 -0300

    12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -2,6 +2,30 @@ const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
 
+/* DELETE todo. */
+router.delete('/', async (req, res) => {
+  await req.todo.delete()  
+  res.sendStatus(200);
+});
+
+/* GET todo. */
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const todo = await Todo.findById(id)
+  console.log({id, todo})
+  if (!todo) res.sendStatus(404); 
+  res.status(200).json(todo)
+});
+
+/* PUT todo. */
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  console.log(req.body)
+  const todo = await Todo.findByIdAndUpdate(id, {...req.body}, {new: true})
+  if (!todo) res.sendStatus(404); 
+  res.status(200).json(todo)
+});
+
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
@@ -16,34 +40,5 @@ router.post('/', async (req, res) => {
   })
   res.send(todo);
 });
-
-const singleRouter = express.Router();
-
-const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
-
-  next()
-}
-
-/* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
-
-/* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
-
-/* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
-
-router.use('/:id', findByIdMiddleware, singleRouter)
-
 
 module.exports = router;
