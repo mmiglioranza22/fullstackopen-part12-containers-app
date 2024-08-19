c1fe6100e08fb822401a8e2c32a1a1248532a7c3
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-08-18 22:14:43 -0300

    12.20

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -4,4 +4,10 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
 })
 
+// const apiClient = axios.create({
+//   baseURL: import.meta.env.PROD
+//     ? import.meta.env.VITE_BACKEND_URL_PROD
+//     : import.meta.env.VITE_BACKEND_URL,
+// });
+
 export default apiClient
